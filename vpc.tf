69e9aa96bdf56abf25bb40e885911ec64725bfa9
niglev <n.iglev@gmail.com>
2019-06-27 03:34:46 +0000

    Add gateway and security groups

--- vpc.tf
+++ vpc.tf
@@ -30,3 +30,143 @@ resource "aws_subnet" "private-subnet" {
   }
 }
 
+# Define the NAT gateway
+resource "aws_eip" "nat_gw_eip" {
+  vpc = true
+
+  tags {
+    Name = "VPC NGW"
+  }
+}
+
+resource "aws_nat_gateway" "gw" {
+  allocation_id = "${aws_eip.nat_gw_eip.id}"
+  subnet_id     = "${aws_subnet.public-subnet.id}"
+
+  depends_on = ["aws_internet_gateway.gw"]
+
+  tags {
+    Name = "gw NAT"
+  }
+}
+
+# Define Routing table for the NAT network
+resource "aws_route_table" "db-private-rt" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "Main Route Table for NAT-ed subnet"
+  }
+}
+
+resource "aws_route_table_association" "db-private-rt" {
+  subnet_id      = "${aws_subnet.private-subnet.id}"
+  route_table_id = "${aws_route_table.db-private-rt.id}"
+}
+
+# Define the internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "VPC IGW"
+  }
+}
+
+# Define the route table
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "Public Subnet RT"
+  }
+}
+
+# Assign the route table to the public Subnet
+resource "aws_route_table_association" "web-public-rt" {
+  subnet_id      = "${aws_subnet.public-subnet.id}"
+  route_table_id = "${aws_route_table.web-public-rt.id}"
+}
+
+# Define the security group for public subnet
+resource "aws_security_group" "sgweb" {
+  name        = "vpc_wp_web"
+  vpc_id      = "${aws_vpc.default.id}"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags {
+    Name = "Web Server SG"
+  }
+}
+
+# Define the security group for private subnet
+resource "aws_security_group" "sgdb" {
+  name        = "sg_wp_web"
+  vpc_id      = "${aws_vpc.default.id}"
+  description = "Allow traffic from public subnet"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+
+
+  tags {
+    Name = "DB SG"
+  }
+}
